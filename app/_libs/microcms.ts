e4b7c3e5bbc0b7219147f59d673c6437660c513f
sum <yuritaso0204@gmail.com>
2025-05-06 14:28:21 +0900

    下書きプレビュー時はSSRをする

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -54,6 +54,11 @@ export const getNewsDetail = async (contentId: string, queries?: MicroCMSQueries
         endpoint: "news",
         contentId,
         queries,
+        customRequestInit: {
+            next: {
+                revalidate: queries?.draftKey === undefined ? 60 : 0,
+            },
+        },
     });
     return detailData;
 };
